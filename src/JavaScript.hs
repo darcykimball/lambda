8cd30e095f2c856aa50379c7071fd3f83aabee2f
Darcy Kimball <darcy@localhost.localdomain>
2016-06-05 23:35:47 -0700

    Slight refactoring so compiling to other languages'll be easier

--- src/JavaScript.hs
+++ src/JavaScript.hs
@@ -2,35 +2,25 @@ module JavaScript where
 
 import Text.Parsec
 
+import qualified Imperative as I
 import qualified Lambda as L
 
--- Very minimal subset of javascript
---   -only unary functions
---   -only return [expr] as function bodies
-
-data Expr = 
-    FnCall Expr Expr
-  | FnDef String (Maybe String) Expr
-  | Var String
-  deriving (Show, Eq)
-
-type Prog = [Expr]
-
-translate :: String -> Either ParseError Expr
+translate :: String -> Either ParseError I.Expr
 translate = fmap translate' . runParser L.parseTopLevel [] "(Translating to JS)"
 
-translate' :: L.Term L.VarName -> Expr
-translate' (L.Var v) = Var $ v
-translate' (L.Abs arg body) = FnDef arg Nothing (translate' body)
-translate' (L.App t1 t2) = FnCall (translate' t1) (translate' t2)
+translate' :: L.Term L.VarName -> I.Expr
+translate' (L.Var v) = I.Var $ v
+translate' (L.Abs arg body) = I.FnDef arg Nothing (translate' body)
+translate' (L.App t1 t2) = I.FnCall (translate' t1) (translate' t2)
 
 compile :: String -> Either ParseError String
 compile = fmap compile' . translate
 
-compile' :: Expr -> String
-compile' (FnCall e1 e2) = wrapParens (compile' e1) ++ wrapParens (compile' e2)
-compile' (Var v)        = wrapSpace v
-compile' (FnDef arg mname body) =
+compile' :: I.Expr -> String
+compile' (I.FnCall e1 e2) =
+  wrapParens (compile' e1) ++ wrapParens (compile' e2)
+compile' (I.Var v) = wrapSpace v
+compile' (I.FnDef arg mname body) =
   maybe "" (++ " = ") mname ++ "function" ++
     (wrapSpace . wrapParens) arg ++ block ("return " ++ compile' body ++ ";")
 
