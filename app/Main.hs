722d9605700b44d13030b94bd4c084d57ef66c49
Darcy Kimball <darcy@localhost.localdomain>
2016-06-07 22:52:54 -0700

    Added some command-line parsing stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+import Options.Applicative
 import System.IO
 
 import JavaScript
 
 main :: IO ()
-main = undefined
+main = execParser opts >>= print
+  where
+    opts = info (helper <*> lang) (fullDesc <> progDesc desc)
+    desc = "Translate some lambda terms into JavaScript, Python, or Perl."
+  
+data Lang = JavaScript | Python | Perl deriving (Eq, Show, Read)
+
+lang :: Parser Lang
+lang = option auto
+         (long "lang" 
+          <> short 'l' 
+          <> metavar "LANGUAGE" 
+          <> value JavaScript
+          <> help "Target language")
